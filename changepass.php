51180bd4b960f9fdb4106821becedafc4906c056
jtanuwidjaja <32887902+jtanuwidjaja@users.noreply.github.com>
2017-10-19 10:01:58 +1300

    Add files via upload
    ffggfggfghf

---
+++ changepass.php
@@ -0,0 +1,155 @@
+<?php
+    include("loginserv.php");
+    include("edit_user.php");
+    
+?>
+
+
+<!doctype html>
+
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Personal information - MRBS</title>
+	     <link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/mobile.css" media="screen and (max-width : 568px)">
+    
+	 <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+	
+        <!-- Static navbar -->
+    <nav class="navbar navbar-inverse">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php"><img src="images/MRBSicon.png" width="100px" height="25px" alt="">
+          </a>
+
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="gallery.php">Gallery</a></li>
+            
+            <?php if (isset ($_SESSION["role"])) { echo '
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Booking<span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="booking.php">Book room</a></li>';}
+                if (($_SESSION["role"] == "staff")||($_SESSION["role"] == "administrator")){
+            echo '
+                <li><a href="booking_list.php">Booking list</a></li>'; }
+                if (isset ($_SESSION["role"])) { echo '                 
+                <li role="separator" class="divider"></li>
+                <li><a href="cancel_booking.php">Cancel booking</a></li>
+              </ul>
+            </li>'; } ?>
+            <?php if ($_SESSION["role"] == "administrator"){
+            echo '
+            <li><a href="user.php">User List</a></li>'; } ?>
+            
+                                                                                                
+            <li><a href="contact.php">Contact us</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <?php if (isset($_SESSION["name"])) { echo '
+            <li class="active"><a href="userinfo.php?ID='.$_SESSION["userid"].'">'.$_SESSION["name"].'</a></li>
+            <li><a href="index.php?logout=1"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>'; }
+            else {
+                echo '
+            <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+            <li><a href="registration.php"><span class="glyphicon glyphicon-user"></span> Register</a></li>'; }?>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+	
+    
+   
+       
+    
+        <div class="container"> 
+       <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">Change password
+<!--                    <small>We are always ready to help you</small>-->
+                </h1>
+                <p>To change password please enter your old password</p>
+            </div>
+       </div>
+       
+    
+        <form action="" method="post" onchange="checkform()" role="form">
+           <div class="row"> 
+                <div class="form-group col-lg-4">
+                    <label >Username*</label>
+                    <input type="text" class="form-control rq" id="userid" name="userid" placeholder="Enter username" readonly <?php echo 'value='.$_SESSION["userid"]; ?>>
+                </div>
+           </div>
+            <div class="row"> 
+                <div class="form-group col-lg-4">
+                    <label>Old Password</label>
+                    <input type="password" class="form-control rq"  id="passsword" name="old_password" placeholder="Enter password">
+                </div>
+            </div>   
+            <div class="row"> 
+                <div class="form-group col-lg-4">
+                    <label>New Password</label>
+                    <input type="password" class="form-control rq"  id="passsword" name="password" placeholder="Enter password">
+                </div>
+            </div>
+            
+            <div class="row col-lg-4">
+                <button type="submit" class="btn btn-primary" name="updatepass" id="signup" disabled>Update</button>
+            </div>
+
+                    <!-- Error Message -->
+                    <br><br>
+                    <span style="color:red;font-size=20px;"><?php echo $error; ?></span>
+        </form>
+       </div>    
+        
+  <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <footer id="myFooter">
+        
+        <br>
+        <div class="text-center">
+        <p>Find us in social networks<p>
+        <a onclick="" class="btn btn-social-icon btn-lg btn-facebook"><i class="fa fa-facebook"></i></a>
+        <a onclick="" class="btn btn-social-icon btn-lg btn-instagram"><i class="fa fa-instagram"></i></a>
+        <a onclick="" class="btn btn-social-icon btn-lg btn-linkedin"><i class="fa fa-linkedin"></i></a>
+    </div>
+        <div class="footer-copyright">
+            <p>© 2017 Copyright Andrey Dementyev</p>
+        </div>
+        
+        
+    </footer>
+    
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>    
+      
+    <script src="js/site.js" type="text/javascript"></script> 
+        
+    
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>	
+    
+</body>
+</html>
